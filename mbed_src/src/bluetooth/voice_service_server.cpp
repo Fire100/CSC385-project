7e73b31b3bfaf2350be9c9cbc171603aba40f183
Fire100 <ilancito70@gmai.com>
2024-04-01 12:58:46 -0400

    Remvoe print statements

--- mbed_src/src/bluetooth/voice_service_server.cpp
+++ mbed_src/src/bluetooth/voice_service_server.cpp
@@ -15,19 +15,6 @@ VoiceServiceServer::VoiceServiceServer()
 
     BLE &ble = BLE::Instance();
 
-    // GattAttribute* nameDescrStart = new GattAttribute( BLE_UUID_DESCRIPTOR_CHAR_USER_DESC , (uint8_t *)start_name.c_str(), start_name.size());
-    // GattAttribute *descriptorsStart[] = {nameDescrStart};
-
-    // GattAttribute* nameDescrRECEIVEAudio = new GattAttribute( BLE_UUID_DESCRIPTOR_CHAR_USER_DESC , (uint8_t *)received_name.c_str(), received_name.size());
-    // GattAttribute *descriptorsRECEIVEdAudio[] = {nameDescrRECEIVEAudio};
-
-    // GattAttribute* nameDescrSentAudio = new GattAttribute( BLE_UUID_DESCRIPTOR_CHAR_USER_DESC , (uint8_t *)sent_name.c_str(), sent_name.size());
-    // GattAttribute *descriptorsSentAudio[] = {nameDescrSentAudio};
-
-    // VOICESERVICE_START = new ReadOnlyGattCharacteristic<uint8_t> (VOICESERVICE_START_UUID, 0, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, descriptorsStart,  sizeof(descriptorsStart)/sizeof(GattAttribute*));
-    // VOICESERVICE_RECEIVE_AUDIO = new WriteOnlyArrayGattCharacteristic<uint8_t, AUDIO_TRANSFER_SIZE> (VOICESERVICE_RECEIVE_AUDIO_UUID, 0, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, descriptorsRECEIVEdAudio, sizeof(descriptorsRECEIVEdAudio)/sizeof(GattAttribute*));
-    // VOICESERVICE_SEND_AUDIO = new ReadOnlyArrayGattCharacteristic<uint8_t, AUDIO_TRANSFER_SIZE> (VOICESERVICE_SEND_AUDIO_UUID, 0, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, descriptorsSentAudio, sizeof(descriptorsSentAudio)/sizeof(GattAttribute*));
-    
     VOICESERVICE_START = new ReadOnlyGattCharacteristic<uint8_t> (VOICESERVICE_START_UUID, 0, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY);
     VOICESERVICE_RECEIVE_AUDIO = new ReadWriteArrayGattCharacteristic<uint8_t, AUDIO_TRANSFER_SIZE> (VOICESERVICE_RECEIVE_AUDIO_UUID, 0, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE_WITHOUT_RESPONSE);
     VOICESERVICE_SEND_AUDIO = new ReadWriteArrayGattCharacteristic<uint8_t, AUDIO_TRANSFER_SIZE> (VOICESERVICE_SEND_AUDIO_UUID, 0, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE_WITHOUT_RESPONSE);
@@ -50,16 +37,6 @@ void VoiceServiceServer::start()
     ble.gattServer().addService(voice_service_service);
 
     ble.gattServer().setEventHandler(this);
-    
-
-    // mainQueue.call_every()
-
-    // This is test code. remove this when audio setup is done
-    // uint16_t start = 12;
-    // uint16_t audio = 30;
-    // ble::GattServer& server = ble.gattServer();
-    // server.write(VOICESERVICE_START->getValueHandle(), (uint8_t*)&start, sizeof(start));
-    // server.write(VOICESERVICE_RECEIVE_AUDIO->getValueHandle(), (uint8_t*)&audio, sizeof(audio));
 };
 
 
@@ -73,6 +50,7 @@ void VoiceServiceServer::sendAudio() {
         BLE &ble = BLE::Instance();
         int audioTransferIterations = (int) send_audio_size / AUDIO_TRANSFER_SIZE;
 
+        printf("Sending Audio Data\n");
         ble_error_t err = ble.gattServer().write(VOICESERVICE_SEND_AUDIO->getValueHandle(), (uint8_t *)&send_audio_data[this->buffer_location * AUDIO_TRANSFER_SIZE], sizeof(send_audio_data[0]) * AUDIO_TRANSFER_SIZE);
         this->buffer_location++;
 
@@ -90,19 +68,13 @@ void VoiceServiceServer::onDataWritten(const GattWriteCallbackParams &params) {
         voiceService->receiving_audio = 1;
         voiceService->hvx_count++;
         
-        // if (audio_buffer_idx == 0){
-        //     t.start();
-        // }
-
-        // this->playAudio((uint8_t*)params.data, AUDIO_TRANSFER_SIZE);
-        
         int to_copy = min((int)AUDIO_TRANSFER_SIZE, voiceService->audio_buffer_size - (int)audio_buffer_idx);
         memcpy(audio_buffer + audio_buffer_idx, params.data, to_copy);
 
         audio_buffer_idx += to_copy;
         printf("Audio Data Index: %d\n", audio_buffer_idx);
+
         if (audio_buffer_idx >= voiceService->audio_buffer_size){
-            // t.stop();
             this->playAudio(audio_buffer, voiceService->audio_buffer_size);
             audio_buffer_idx = 0;
         }
